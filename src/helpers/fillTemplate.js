5546bf58547522855d47888bbf915af88c1dc5d4
yoksel <yokselzok@gmail.com>
2020-01-14 21:14:07 +0300

    Add some helpers

---
+++ src/helpers/fillTemplate.js
@@ -0,0 +1,12 @@
+export default function fillTemplate({tmpl, data}) {
+  if(!tmpl || !data) {
+    return;
+  }
+
+  return tmpl.replace(/{([^"]{1,})}/g, (match, str) => {
+    if (data[str]) {
+      return data[str];
+    }
+    return '';
+  });
+}
