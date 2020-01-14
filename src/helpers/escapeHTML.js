a9d1df03784fc57dc8db03609c7cddc8549658b6
yoksel <Yuliya.Buhvalova@rambler-co.ru>
2020-01-14 17:13:09 +0300

    Add helpers

---
+++ src/helpers/escapeHTML.js
@@ -0,0 +1,8 @@
+export default function escapeHTML(string) {
+  return string
+  .replace(/&/g, '&')
+  .replace(/"/g, '&quot;')
+  .replace(/'/g, '&#39;')
+  .replace(/</g, '<')
+  .replace(/>/g, '>');
+}
---
+++ src/helpers/formatDate.js
@@ -0,0 +1,9 @@
+export default function formatDate(str) {
+  const date = new Date(str).toDateString();
+  const dataParts = date.split(' ');
+  const month = dataParts[1];
+  const day = dataParts[2];
+  const year = dataParts[3];
+
+  return `${month} ${day}, ${year}`;
+}
