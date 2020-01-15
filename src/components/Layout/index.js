e1139d8b22bb9f61a7c55cea8f8c2335cceb13c6
yoksel <Yuliya.Buhvalova@rambler-co.ru>
2020-01-15 15:30:00 +0300

    Move links from footer to sidebar; adjust section styles

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -10,19 +10,25 @@ export default class Layout extends HTMLElement  {
     return createElement(`<div class="page">
       <div class="page-container">
         <aside class="page-aside">
-          (navigation will be here)
+          <nav>
+            <ul>
+              <li>
+                <a href="/products">test link</a>
+              </li>
+              <li>
+                <a href="/dashboard">test link</a>
+              </li>
+              <li>
+                <a href="/">test link</a>
+              </li>
+            </ul>
+          </nav>
         </aside>
 
         <main class="page-main" id="content">
           <!-- main content will be here -->
         </main>
       </div>
-
-      <footer class="page-footer">
-        <a href="/products">test link</a>
-        <a href="/dashboard">test link</a>
-        <a href="/">test link</a>
-      </footer>
     </div>`);
   }
 };
