622045e59285d5a4de6c4b79c1eaeb9a7b8d1bd5
yoksel <Yuliya.Buhvalova@rambler-co.ru>
2020-01-16 12:06:52 +0300

    Add products page

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -2,10 +2,23 @@ import {createElement} from '../../helpers/index.js';
 
 export default class {
   async render() {
-    return createElement(`<div>
-        <h1>Products</h1>
-        <p>Hello</p>
-      </div>`
+    return createElement(`<div class="page-content">
+      <header class="page-content__header">
+        <h1 class="page-content__title">Dashboard/Products</h1>
+      </header>
+
+      <section class="page-section">
+        <h2 class="page-section__title">Products</h2>
+
+        <sortable-table
+          data-url="/api/rest/products?_embed=subcategory.category",
+          data-is-dynamic="1"
+          data-fields-list="['images', 'title', 'subcategory', 'quantity', 'price', 'status']"
+          data-order-field='title',
+          data-order-direction="1"
+          ></sortable-table>
+      </section>
+    </div>`
     );
   }
 };
