fc23a6b0726fca8c8a449b444d63ecae50902811
yoksel <Yuliya.Buhvalova@rambler-co.ru>
2020-01-17 15:47:53 +0300

    Fix eslint; add eslint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,11 +9,16 @@ module.exports = {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
   },
+  parser: "babel-eslint",
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module'
   },
   rules: {
-    semi: ['error', 'always']
+    semi: ['error', 'always'],
+    // fix: Cannot read property 'range' of null
+    //https://github.com/babel/babel-eslint/issues/681#issuecomment-420663038
+    'template-curly-spacing' : 'off',
+    indent : "off"
   }
 }
