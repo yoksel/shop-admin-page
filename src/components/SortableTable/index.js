a06537536e6cf01b31d290f4b5fae4501aa7edf7
yoksel <yokselzok@gmail.com>
2020-01-31 10:47:38 +0300

    Add some adaptivity; try to fix table layout

--- src/components/SortableTable/index.js
+++ src/components/SortableTable/index.js
@@ -214,7 +214,7 @@ export default class SortableTable extends HTMLElement {
         const direction = getDirectionText(this.sorting.isAsc);
         data.sortDirection = `data-sort-direction="${direction}"`;
       }
-      headerContent += `<div class="${thClass}"
+      headerContent += `<div class="${thClass} ${thClass}--${field}"
         data-name="${field}"
         ${data.sorter}
         ${data.sortDirection}
