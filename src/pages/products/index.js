a29ac35b17ea370bab18667dfd68d6d142fc18fb
yoksel <yokselzok@gmail.com>
2020-02-09 20:07:56 +0300

    Remove customElements from products page

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,9 +1,16 @@
 import { createElement } from '../../helpers/index.js';
+import Filter from '../../components/Filter/index.js';
+import SortableTable from '../../components/SortableTable/index.js';
+
 const baseUrl = process.env.BASE_URL || '/';
 
 export default class {
   async render () {
-    return createElement(`
+    this.table = this.getTable();
+    this.filter = this.getFilter();
+    this.filter.updateProps = this.table.updateProps;
+
+    const page = createElement(`
 <div class="page-content">
   <header class="page-content__header">
     <h2 class="page-content__title">Products</h2>
@@ -13,23 +20,34 @@ export default class {
       href="${baseUrl}products/add">Add Product
     </a>
   </header>
-
-  <table-filter
-    data-fields-list="['search', 'price', 'status']"
-    data-target-selector="#products-table"
-    ></table-filter>
-
-  <sortable-table
-    id="products-table"
-    data-url="/api/rest/products?_embed=subcategory.category",
-    data-query-params=''
-    data-is-dynamic="1"
-    data-fields-list="['images', 'title', 'subcategory', 'quantity', 'price', 'status']"
-    data-order-field='title',
-    data-order-direction="1"
-    ></sortable-table>
-  </section>
 </div>
   `);
+
+    page.append(this.filter.elem);
+    page.append(this.table.elem);
+
+    return page;
+  }
+
+  getTable () {
+    const table = new SortableTable({
+      id: 'products-table',
+      url: '/api/rest/products?_embed=subcategory.category',
+      isDynamic: 1,
+      fieldsList: ['images', 'title', 'subcategory', 'quantity', 'price', 'status'],
+      orderField: 'title',
+      orderDirection: 1
+    });
+
+    return table;
+  }
+
+  getFilter () {
+    const filter = new Filter({
+      targetElem: this.filterElem,
+      fieldsList: ['search', 'price', 'status']
+    });
+
+    return filter;
   }
 }
