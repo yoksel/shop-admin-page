c6ac93427639b85453611ed5a010ebc961b262f9
yoksel <yokselzok@gmail.com>
2020-02-10 08:54:18 +0300

    Remove needless file

--- src/helpers/getDateFromString.js
+++
@@ -1,4 +0,0 @@
-export default function getDateFromString (str) {
-  const [day, month, year] = str.split('/');
-  return new Date(year, month - 1, day);
-};
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,7 +3,6 @@ import escapeHTML from './escapeHTML.js';
 import fillTemplate from './fillTemplate.js';
 import formatDate from './formatDate.js';
 import formatTotal from './formatTotal.js';
-import getDateFromString from './getDateFromString.js';
 import getWeekDays from './getWeekDays.js';
 import throttle from './throttle.js';
 import fetchJson from './fetchJson.js';
@@ -14,7 +13,6 @@ export {
   fillTemplate,
   formatDate,
   formatTotal,
-  getDateFromString,
   getWeekDays,
   throttle,
   fetchJson
